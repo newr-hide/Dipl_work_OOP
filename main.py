e4145f99e07fac47e20050463dfe6ef53f4da019
Bondarev Artem <bond210484@mail.ru>
2024-04-28 08:00:43 +0700

    add progressBar and requirements

--- main.py
+++ main.py
@@ -1,95 +1,82 @@
 import requests
-from pprint import pprint
-import json
-class VK:
-    def __init__(self, access_token, user_id, version='5.131'):
-        self.token = access_token
-        self.id = user_id
-        self.version = version
-        self.params = {'access_token': self.token, 'v': self.version}
+from datetime import datetime as dt
+import time
+from tqdm import tqdm
 
-    def users_info(self):
-        url = 'https://api.vk.com/method/users.get'
-        params = {'user_ids': self.id}
-        response = requests.get(url, params={**self.params, **params})
-        return response.json()
-    def photos_get(self):
-        url = 'https://api.vk.com/method/photos.get'
-        params = {
-            'owner_id': '31616399', 'album_id': "profile", 'extended': 1
-        }
-        response = requests.get(url, params={**self.params, **params})
-        return response.json()
+def copy_photo_from_VK_in_YaD(VK_id, access_token_VK, token_YAD, album="profile" ):
 
 
-access_token = 'vk1.a.dvYoKf9Kkke38EHfw1q1D1SyPlp6-EDrMwR41yLQ2ghnNj0VyPGmW-c9yQsjIdXKnAgTzWZLiV_gBKhLrp1LmlhBrIX8zBEl3eY4zr3ucyspCbJ3gnjCEf_QK9AG9uFlJ-TchYceU6txXbGUP9K_ecRHqPfOTpkgak-PU6lylYBpSQFTwz-ySbbsVL3cOXbd'
-user_id = 'wwc21'
-vk = VK(access_token, user_id)
+    class VK:
+        def __init__(self, access_token, user_id, version='5.131'):
+            self.token = access_token
+            self.id = user_id
+            self.version = version
+            self.params = {'access_token': self.token, 'v': self.version}
 
-tmp_vk = vk.photos_get()["response"]['items']
+        def users_info(self):
+            url = 'https://api.vk.com/method/users.get'
+            params = {'user_ids': self.id}
+            response = requests.get(url, params={**self.params, **params})
+            return response.json()
 
-for i in tmp_vk:
-    name_photo = i['likes']['count']
-    for j in i['sizes']:
-        if j['type'] == 'w':
-            url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources/'
+        def photos_get(self):
+            url = 'https://api.vk.com/method/photos.get'
             params = {
-                'path': 'Photo_VK'
+                'owner_id': '31616399', 'album_id': album, 'extended': 1
             }
-            headers = {
-                'Authorization': 'OAuth y0_AgAAAAADBqHNAADLWwAAAAECiSyaAABbM4VSlj5OaZsg5MnbEw08iODqDg',
-                'Accept': 'application/json', 'Content-Type': 'application/json'
-            }
-
-            response = requests.put(url_create_folder,
-                                    params=params,
-                                    headers=headers)
-
-            # послать картинку на ЯД
-            params = {
-                'path': f'Photo_VK/{name_photo}',
-                'url': j['url']
-            }
-
-            response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
-                                     headers=headers,
-                                     params=params)
-            print(response.json())
+            response = requests.get(url, params={**self.params, **params})
+            return response.json()
 
 
+    access_token = access_token_VK
+    user_id = VK_id
+    vk = VK(access_token, user_id)
+    print(vk.photos_get())
+    tmp_vk = vk.photos_get()["response"]['items']
+
+    def _add_date_to_file_name(file_name):
+        date_str = dt.now().strftime('%Y-%m-%d_%H-%M-%S')
+        new_file_name = str(file_name) + '_' + str(date_str)
+        return new_file_name
 
 
+    tmp_name = ''
 
 
+    for i in tqdm(tmp_vk):
+
+        name_photo = i['likes']['count']
+        for j in i['sizes']:
+            if j['type'] == 'w':
+                url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources/'
+                params = {
+                    'path': 'Photo_VK'
+                }
+                headers = {
+                    'Authorization': token_YAD,
+                    'Accept': 'application/json', 'Content-Type': 'application/json'
+                }
+
+                response = requests.put(url_create_folder,
+                                        params=params,
+                                        headers=headers)
+                if name_photo == tmp_name:
+
+                    params = {
+                        'path': f'Photo_VK/{_add_date_to_file_name(name_photo)}',
+                        'url': j['url']
+                    }
+                else:
+                    params = {
+                        'path': f'Photo_VK/{name_photo}',
+                        'url': j['url']
+                    }
+
+                tmp_name = name_photo
+                response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                                        headers=headers,
+                                        params=params)
+                time.sleep(1)
+                print(response.json())
 
 
-
-# url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
-# params = {
-#     'path': 'Image_VK'
-# }
-# headers = {
-#     'Authorization': 'OAuth y0_AgAAAAADBqHNAADLWwAAAAECiSyaAABbM4VSlj5OaZsg5MnbEw08iODqDg',
-#     'Accept': 'application/json', 'Content-Type': 'application/json'
-#            }
-#
-# response = requests.put(url_create_folder,
-#                         params=params,
-#                         headers=headers)
-#
-# # послать картинку на ЯД
-# params = {
-#     'path': 'Image_VK',
-#     'url': tmp_lines
-# }
-#
-# response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
-#                         headers=headers,
-#                         params=params)
-# print(response.json())
-# url_for_upload = response.json()['href']
-#
-# with open(image_name, 'rb') as f:
-#     requests.put(url_for_upload,
-#                  files={'file': f})
-#     print(response)
